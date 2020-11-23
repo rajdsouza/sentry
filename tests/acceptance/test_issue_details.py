b7292c7addb47d9d76ba2493cffb836a69c6fae2
Billy Vong <billy@sentry.io>
2020-11-23 10:36:45 -0800

    test(acceptance): Fix flaky "issue details python" snapshot
    This is the only issue details snapshot that is flaky (it happens with the "last seen" marker on the sidebar). The other tests dont use this `time` arg, so it should be ok to remove

--- tests/acceptance/test_issue_details.py
+++ tests/acceptance/test_issue_details.py
@@ -52,7 +52,7 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
         return event
 
     def test_python_event(self):
-        event = self.create_sample_event(platform="python", time=event_time)
+        event = self.create_sample_event(platform="python")
         self.page.visit_issue(self.org.slug, event.group.id)
 
         # Wait for tag bars to load
